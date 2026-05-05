77223056b5f57c031de614b5d31d5351fb4ce1a7
Ivan Kabanovskyi <kabanovskiy.ivan@gmail.com>
2026-05-05 23:31:31 +0300

    test

--- diplom/back-end/Znaichyk/Znaichyk/wsgi.py
+++ diplom/back-end/Znaichyk/Znaichyk/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for Znaichyk project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# Повний шлях до папки, де лежить manage.py
+path = '/home/diplomznaichyk/Znaichyk/diplom/back-end/Znaichyk'
+if path not in sys.path:
+    sys.path.append(path)
+
+# Вказуємо шлях до settings.py
+# Примітка: 'znaichyk' — це назва папки всередині, де лежить settings.py
+os.environ['DJANGO_SETTINGS_MODULE'] = 'znaichyk.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Znaichyk.settings')
-
 application = get_wsgi_application()
